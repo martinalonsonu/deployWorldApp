1b3903ed5ae51e73875906308bf5dcc971482a51
Martin Alonso Núñez Navarro <114968362+martinalonsonu@users.noreply.github.com>
2023-12-27 18:03:59 -0500

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
     DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, POSTGRES_URL
 } = process.env;
 
-const sequelize = new Sequelize(`POSTGRES_URL`, {
+const sequelize = new Sequelize(POSTGRES_URL, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
