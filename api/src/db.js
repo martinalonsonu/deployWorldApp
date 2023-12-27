2145a7da196446354d86a85f461bf159f3af69dc
Martin Alonso Núñez Navarro <114968362+martinalonsonu@users.noreply.github.com>
2023-12-27 17:44:36 -0500

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, POSTGRES_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://default:Rif5on8XqFvj@ep-bold-mode-52864691-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb`, {
+const sequelize = new Sequelize(`POSTGRES_URL`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
