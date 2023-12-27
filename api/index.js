d04ec304e4287fe576e7ba6d8ad7c3de5a32d4b7
Martin Alonso Núñez Navarro <114968362+martinalonsonu@users.noreply.github.com>
2023-12-27 16:26:14 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 
 // Syncing all the models at once.
-conn.sync({ force: false })
+conn.sync({ force: true })
     .then(async () => {
         try {
             await dataApi();
